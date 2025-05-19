0c2df122eef04651f632f03e2de16e7f1d474b2a
pawel2557 <pawelnowak984@gmail.com>
2025-05-19 14:21:10 +0200

    added small bug fixes

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,3 +2,4 @@ export { Header } from "./Layouts/Header";
 export { Footer } from "./Layouts/Footer";
 
 export { ProductCard } from "./Elements/ProductCard";
+export { ScrollToTop } from "./Other/ScrollToTop";
